13e212b668f17f401b7ae9554f4d0b4d8f6c921c
HariKrishna-A <arulhari333@gmail.com>
2019-12-22 10:02:46 +0530

    BD

---
+++ ASSESSMENT/FINAL_ASSESSMENT/STOCK_BD/src/main/java/com/ustglobal/assessmentboot/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.ustglobal.assessmentboot.service;
+
+import java.util.List;
+
+import com.ustglobal.assessmentboot.dto.OrderInfo;
+import com.ustglobal.assessmentboot.dto.ProductsInfo;
+
+public interface ProductService {
+	
+	public ProductsInfo searchProductById(int pid);
+	public boolean modifyProduct(ProductsInfo info);
+	public boolean addProduct(ProductsInfo info);
+	public boolean addToCart(ProductsInfo info);
+	public List<ProductsInfo> getAllProducts();
+	public List<OrderInfo> getFromCart();
+	
+}
